280ea85347547ee3d2708671625047ed19d4352e
daladno13 <slickindub@gmail.com>
2020-08-04 16:14:46 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20,
+    backSpeed: 5,
+    startDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+  });
+
+});
